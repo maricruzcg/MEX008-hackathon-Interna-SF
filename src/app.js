821806d23a616c4974a0caa73b753d60a00fef9c
Isis Lázaro <isislazaro@gmail.com>
2019-07-25 10:48:57 -0500

    se cambiaron estilos vianey e isis

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
-//alert('funcionaaa');
+//Prueba sweet alert
+document.getElementById('modal-ayuda').addEventListener('click', ()=> {
+    Swal.fire({
+        type: 'error',
+        title: 'Oops...',
+        text: 'Something went wrong!',
+        footer: '<a href>Why do I have this issue?</a>'
+      })
+});
+
 btnDefMeta   = document.getElementById("define-tu-meta");
 btnCalcula   = document.getElementById("btn-calcula");
 scrnIntro    = document.getElementById("scrn-intro");
 scrnCalcula  = document.getElementById("scrn-calcula");
 formCalcula  = document.getElementById("form-calcula");
 ahorro       = document.getElementById("ingresa-ahorro");
-gastoSemanal = document.getElementById("ingresa-gasto-semanal");
+gastoSemanal = document.getElementById("ingresa-gasto");
 
 btnDefMeta.addEventListener("click", () => {
     scrnCalcula.style.display = "block";
