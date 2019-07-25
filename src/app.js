7a167ed5e6c8a69e187396cae3c72b6046854e24
Isis Lázaro <isislazaro@gmail.com>
2019-07-24 23:07:15 -0500

    Se ocultó pantalla calcula (css), y se habilitó button para que apareciera y realizar cálculos (js). Se habilitó la introducción obligatoria de datos numéricos para obtener datos y poder realizar el cálculo

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
 //alert('funcionaaa');
+btnDefMeta = document.getElementById("define-tu-meta");
+btnCalcula = document.getElementById("btn-calcula");
+scrnIntro = document.getElementById("scrn-intro");
+scrnCalcula = document.getElementById("scrn-calcula");
+
+btnDefMeta.addEventListener("click", () => {
+    scrnCalcula.style.display = "block";
+    scrnIntro.style.display = "none";
+});
