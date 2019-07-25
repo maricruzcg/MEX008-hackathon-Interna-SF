cef93766bbebec5946ee424bd40a6f70435d3fb9
Isis Lázaro <isislazaro@gmail.com>
2019-07-25 00:50:40 -0500

    la web app ya calcula el saldo del usuario, utiliza la función budgetCalc que se encuentra en presupuesto.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,19 @@
 //alert('funcionaaa');
-btnDefMeta = document.getElementById("define-tu-meta");
-btnCalcula = document.getElementById("btn-calcula");
-scrnIntro = document.getElementById("scrn-intro");
-scrnCalcula = document.getElementById("scrn-calcula");
+btnDefMeta   = document.getElementById("define-tu-meta");
+btnCalcula   = document.getElementById("btn-calcula");
+scrnIntro    = document.getElementById("scrn-intro");
+scrnCalcula  = document.getElementById("scrn-calcula");
+formCalcula  = document.getElementById("form-calcula");
+ahorro       = document.getElementById("ingresa-ahorro");
+gastoSemanal = document.getElementById("ingresa-gasto-semanal");
 
 btnDefMeta.addEventListener("click", () => {
     scrnCalcula.style.display = "block";
     scrnIntro.style.display = "none";
+    ahorro.focus();
+});
+
+formCalcula.addEventListener("submit", event => {
+    event.preventDefault();
+    console.log(presupuesto.budgetCalc(parseInt(ahorro.value), parseInt(gastoSemanal.value)));
 });
